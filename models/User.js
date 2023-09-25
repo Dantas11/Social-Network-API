acc1d9f64cd6932b7b14923bb6a27a29c973d6ce
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-09-25 18:21:16 +1000

    code added to user schema in models

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendsCount").get(function () {
+  return this.friends !== undefined ? this.friends.length : 0;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
