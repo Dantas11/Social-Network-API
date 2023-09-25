cee28c6f2c6bb4b6550691b6d11f768cd32da684
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-09-25 19:19:30 +1000

    addded code to thought.js in models

---
+++ models/Thought.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+const { DateTime } = require("luxon");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: { type: String, required: true, maxLength: 280 },
+    username: { type: String, required: true },
+    createdAt: {
+      type: Date,
+      default: DateTime.now(),
+      get: formatedDate,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+function formatedDate(createdAt) {
+  return createdAt.toLocaleString(DateTime.DATE_MED);
+}
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+    createdAt: { type: Date, default: DateTime.now(), get: formatedDate },
+    username: { type: String, required: true },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions !== undefined ? this.reactions.length : 0;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
